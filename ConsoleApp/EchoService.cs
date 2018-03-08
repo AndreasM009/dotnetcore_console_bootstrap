cf9e481ed75c46f013154d699db6e1063112d969
Andreas Mock <anmock@microsoft.com>
2018-03-08 12:59:05 +0100

    Added Boilerplate code

---
+++ ConsoleApp/EchoService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class EchoService : IEchoService
+    {
+        private readonly MyConfiguration _config;
+
+        public EchoService(IOptions<MyConfiguration> options)
+        {
+            _config = options.Value;
+        }
+
+        void IEchoService.Echo()
+        {
+            Console.WriteLine(_config.Title);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false);
+            var configuration = configurationBuilder.Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton(new LoggerFactory().AddConsole().AddDebug())
+                .AddOptions()
+                .Configure<MyConfiguration>(configuration.GetSection("MyConfiguration"))
+                .AddSingleton<IEchoService, EchoService>();
+
+            var services = serviceProvider.BuildServiceProvider();
+            var echo = services.GetService<IEchoService>();
+            echo.Echo();
+
+
+        }
+    }
+}
